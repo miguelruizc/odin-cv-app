cb9d733f94f15592168193e2261632b97f7a1348
miguelruizc <buumchakalaka@gmail.com>
2024-06-15 13:00:42 +0200

    Add general structure with main elements

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,28 @@ function App() {
 
   return (
     <>
-      <Section title="General Information">{}</Section>
-      <Section title="Education">{}</Section>
-      <Section title="Professional Experience">{}</Section>
+      <Section title="General Information">
+        -name
+        <input></input>
+        -email
+        <input></input>
+        -phone
+        <input></input>
+      </Section>
+      <Section title="Education">
+        -education item 
+          -school name
+          -title of study
+          -date of study
+      </Section>
+      <Section title="Professional Experience">
+        -professional item 
+          -company name
+          -position title 
+          -main responsabilities
+          -start date
+          -end date
+      </Section>
     </>
   )
 }
