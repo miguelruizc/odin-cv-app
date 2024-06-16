5a2485b211ac1991ed95af9202a2d843307a562e
miguelruizc <buumchakalaka@gmail.com>
2024-06-16 17:11:36 +0200

    Fix edit/submit toggle to correctly preserve content

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,21 +1,34 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import { React, useState } from 'react';
 import formData from './formData';
 
 function Input({ title, formField, subObject, subObjectId }) {
+	// Get corresponding form value
+	let currentValue = '';
+	if (subObject) {
+		const object = formData[subObject].find((obj) => obj.id === subObjectId);
+		currentValue = object[formField];
+	} else {
+		currentValue = formData[formField];
+	}
+
+	const [inputText, setInputText] = useState(currentValue);
+
 	function updateForm(event) {
 		if (subObject) {
 			const object = formData[subObject].find((obj) => obj.id === subObjectId);
 			object[formField] = event.target.value;
+			setInputText(event.target.value);
 		} else {
 			formData[formField] = event.target.value;
+			setInputText(event.target.value);
 		}
 	}
 
 	return (
 		<div className="formInput">
 			<label htmlFor={`${title}Input`}>{title}</label>
-			<input id={`${title}Input`} onChange={updateForm} />
+			<input type="text" value={inputText} id={`${title}Input`} onChange={updateForm} />
 		</div>
 	);
 }
